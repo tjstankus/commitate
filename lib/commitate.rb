91a97dedcf14d552ca17abd6654d50aa75f940d6
TJ Stankus <tjstankus@gmail.com>
2014-05-29 08:21:41 -0400

    Move regex and configuration constants to module scope

--- lib/commitate.rb
+++ lib/commitate.rb
@@ -4,5 +4,10 @@ require 'commitate/processor'
 require 'commitate/renderer'
 
 module Commitate
-  # Your code goes here...
+  MIN_SHA_LEN = 4
+
+  COMMITATE_REGEX = /^>>                      # start with 2 right angles chars
+                     \[(.+)\]                 # filename in brackets
+                     \((.{#{MIN_SHA_LEN},})\) # commit sha in parens
+                    /x
 end
--- lib/commitate/renderer.rb
+++ lib/commitate/renderer.rb
@@ -3,12 +3,6 @@ require_relative 'processor'
 
 module Commitate
   class Renderer < Redcarpet::Render::HTML
-    MIN_SHA_LEN = 4
-
-    COMMITATE_REGEX = /^>>                      # start with 2 right angles chars
-                       \[(.+)\]                 # filename in brackets
-                       \((.{#{MIN_SHA_LEN},})\) # commit sha in parens
-                      /x
 
     attr_accessor :processor
 
